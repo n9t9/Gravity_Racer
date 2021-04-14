255d2fdb40bbcf888de30e3f6922f8c784ba4c2a
n9t9 <45164557+n9t9@users.noreply.github.com>
2021-04-14 14:51:11 +0530

    Add files via upload

---
+++ Assets/CarControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControls : MonoBehaviour
+{
+    public Rigidbody2D frontTire;
+    public Rigidbody2D backTire;
+    public Rigidbody2D car;
+    public float speed;
+    public float movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+        car.AddTorque(-movement * speed * Time.fixedDeltaTime);
+    }
+
+}
